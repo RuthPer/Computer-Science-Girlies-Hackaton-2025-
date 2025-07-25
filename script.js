16d9899f4f39aafecd3a17325b62728d95fbc9a9
RuthPer <rp104@wellesley.edu>
2025-07-25 18:52:13 -0500

    current progress

--- script.js
+++ script.js
@@ -1,34 +1,8 @@
-async function getData() {
-    
-    const url= "https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&hourly=temperature_2m"
-
-    const res= await fetch(url)
-    const record= await res.json()
-
-    let temps = record.hourly.temperature_2m;
-    let times = record.hourly.time;
-
-    console.log(temps[0]*(9/5)+32)
-    console.log(times[0])
 
 
-    // Showing it on the web page and how it works 
+async function getLoc(){
 
-    let display="";
-
-
-    for (let i=0; i<3; i++){
-
-        display+= `<li>`+ ` ${times[i]} will be ${temps[i]}` + `</li>`
-    }
-
-    document.getElementById("weather").innerHTML= display;
-}
-
-getData();
-
-async function getLocandWeather(){
-
+    // This gets the users current location
     function success(position){
         const latitude=position.coords.latitude;
         const longitude=position.coords.longitude;
@@ -38,18 +12,78 @@ async function getLocandWeather(){
         console.log(longitude)
         console.log(latitude)
 
+        // Used for testing to check to see if location was being pulled
         data= `<li> Lad: ${latitude} and Long: ${longitude} </li>`
-        document.getElementById("ellongandellad").innerHTML=data;
         
+        getWeather(longitude,latitude);
         
-
     }
     
     navigator.geolocation.getCurrentPosition(success);
 
+}
+
+async function getWeather(longitude,latitude){
+    const url= `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&hourly=temperature_2m`
+
+            // Pulling from the weather API
+            const res=  await fetch(url)
+            const record=  await res.json()
+
+            const temps = record.hourly.temperature_2m;
+            const times = record.hourly.time;
+
+            //The current time 
+            const now= new Date();
+            const hours= now.getHours();
+            const timeString = now.toLocaleTimeString();
+
+            console.log(`Current time ${hours}`)
+            let intHours = parseInt(hours);
+
+            //Test Code for API call
+            console.log(temps[intHours]*(9/5)+32)
+            console.log(times[intHours])
+
+
+            // Showing it on the web page and how it works 
+
+            let display="";
+
+
+            document.getElementById("time").innerHTML= `Current time: ${timeString}`;
+            document.getElementById("temp").innerHTML= `Current temp: ${Math.round(temps[intHours ]*(9/5)+32)}`
+            
+
+            // Changes the color of the page based on the current temp at user location
+            // Also display a message 
+            const currentTemp = temps[intHours ]*(9/5)+32;
+
+            if (currentTemp >= 90) {
+                changeBackground("red");
+                document.getElementById("currenttemps").innerHTML="Its way too hot";
+            } else if (currentTemp >=80 ) {
+                changeBackground("orange");
+                document.getElementById("currenttemps").innerHTML="Its not too hot";
+            } else {
+                changeBackground("skyblue");
+                document.getElementById("currenttemps").innerHTML="Its getting chilly";
+            }
+
+    }
     
 
 
+
+
+// Make function that changes the color of the bakcgorund based on how hot it is 
+
+function changeBackground(color){
+    document.body.style.background = color;
+
 }
 
-getLocandWeather();
+
+
+// Starts the extension
+window.addEventListener("load", getLoc);
